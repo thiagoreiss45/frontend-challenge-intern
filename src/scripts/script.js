1bf6fa02eb840e8d8071e53fb56658a92f88725c
Thiago Reis <43996425+thiagoreiss45@users.noreply.github.com>
2020-12-06 18:20:10 -0300

    fix: fetch bugs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,12 +10,14 @@ function createCard(product) {
     card.className = "card"
     card.innerHTML = `
         <img class="cardImg" src=http:${product.image}>
+        <div class="cardBody">
         <div class="cardName">${product.name}</div>
         <div class="cardDesc">${product.description}</div>
         <div class="cardOldPrice">De: R$${product.oldPrice}</div>
         <div class="cardPrice">Por: R$${product.price}</div>
         <div class="cardInstallment">ou ${product.installments.count}x de R$${product.installments.value}</div>
-        <button>Comprar</button>`
+        <button>Comprar</button>
+        </div>`
     selectedProducts.appendChild(card)
 }
 
