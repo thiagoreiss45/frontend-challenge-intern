72eb1cc92b628e66b8133c3a82e4476eeefc12e1
Thiago Reis <43996425+thiagoreiss45@users.noreply.github.com>
2020-12-07 20:02:09 -0300

    add: documentation, form alert sent

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,20 +1,24 @@
+// API URL
 const URL = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
-const selectedProducts = document.getElementById("products")
-let nextPage
 fetchProducts(URL)
 
-document.getElementById('showMoreProducts').addEventListener('click', showMoreProducts)
+const selectedProducts = document.getElementById("products")
+let nextPage
+
+//  Arrow to toggle the Lorem Ipsum text on mobile
 let box = document.getElementById('box'),
-    btn = document.getElementById("hide-button")
-
-
+    btn = document.getElementById("hideButton")
 
 function arrowToggle () {
+
+    //  If is hidden, remove the hidden and visuallyHidden classes
     if (box.classList.contains('hidden')) {
       box.classList.remove('hidden');
       setTimeout(function () {
         box.classList.remove('visuallyhidden');
       }, 20);
+
+    //  Else, add them
     } else {
       box.classList.add('visuallyhidden');    
       box.addEventListener('transitionend', function(e) {
@@ -27,22 +31,31 @@ function arrowToggle () {
     }
     
 }
+
+
+//  Create the HTML product card
 function createCard(product) {
+
+    //  Create a card div
     let card = document.createElement("div")
     card.className = "card"
+    
+    //  Create cardBody to separete cardImg from the rest
     card.innerHTML = `
         <img class="cardImg" src=http:${product.image}>
+        
         <div class="cardBody">
-        <div class="cardName">${product.name}</div>
-        <div class="cardDesc">${product.description}</div>
-        <div class="cardOldPrice">De: R$${product.oldPrice}</div>
-        <div class="cardPrice">Por: R$${product.price}</div>
-        <div class="cardInstallment">ou ${product.installments.count}x de R$${product.installments.value}</div>
-        <button>Comprar</button>
+          <div class="cardName">${product.name}</div>
+          <div class="cardDesc">${product.description}</div>
+          <div class="cardOldPrice">De: R$${product.oldPrice}</div>
+          <div class="cardPrice">Por: R$${product.price}</div>
+          <div class="cardInstallment">ou ${product.installments.count}x de R$${product.installments.value}</div>
+          <button>Comprar</button>
         </div>`
     selectedProducts.appendChild(card)
 }
 
+//  Loads more products
 async function fetchProducts(endpoint) {
     const res = await fetch(endpoint)
     const data = await res.json()
@@ -54,4 +67,19 @@ async function fetchProducts(endpoint) {
 function showMoreProducts(){
     fetchProducts(nextPage)
 }
+
+
+
+//  Alerts you that the information was sent
+function submitInfo(event) {
+  event.preventDefault();
+
+  alert("Formulário enviado!");
+}
+
+//  Add event listeners
+document.getElementById('showMoreProducts').addEventListener('click', showMoreProducts)
 btn.addEventListener('click', arrowToggle,false);
+document.getElementById("sign").addEventListener("submit", submitInfo);
+document.getElementById("news").addEventListener("submit", submitInfo);
+
